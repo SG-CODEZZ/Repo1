e5c24ae388cd8d7f8def0752f2ccbd759978dec1
b_gates <sriramtendulkar@gmail.com>
2021-05-19 01:07:58 +0530

    Welcome Page modified again in Branch1

--- hello_react/src/App.js
+++ hello_react/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React in Branch 1
         </a>
       </header>
     </div>
